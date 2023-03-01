b7fe4cafa41706bd08c25c3000336348243c588a
aslam48 <youngfizzy001@gmail.com>
2023-03-01 14:30:06 +0100

    working

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <TextInput multiline={true} style={styles.textinput}/>
     </View>
   );
 }
@@ -17,4 +18,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  textinput: {
+    backgroundColor: "#ffb70342",
+    width: "100%",
+    height: 200,
+    fontSize: 16,
+    paddingHorizontal: 20,
+  }
 });
